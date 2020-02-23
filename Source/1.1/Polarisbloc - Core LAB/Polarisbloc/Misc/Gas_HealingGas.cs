f2c9d79774e84598226baa3e5c1236855e3b68a3
Vaniat <188733149@qq.com>
2020-02-23 13:29:20 +0800

    Tanslate text update for 1.1.

---
+++ Source/1.1/Polarisbloc - Core LAB/Polarisbloc/Misc/Gas_HealingGas.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+
+namespace Polarisbloc
+{
+    public class Gas_HealingGas : Gas
+    {
+        private int ticks;
+
+        private List<Pawn> initPawns = new List<Pawn>();
+
+        private List<Pawn> Pawns
+        {
+            get
+            {
+                this.initPawns.Clear();
+                foreach (Pawn p in this.PositionHeld.GetThingList(this.MapHeld).FindAll(x => x is Pawn))
+                {
+                    this.initPawns.Add(p);
+                }
+                return this.initPawns;
+            }
+        }
+
+        public override void Tick()
+        {
+            
+            this.ticks++;
+            if (this.ticks >= 60)
+            {
+                this.ticks = 0;
+                foreach (Pawn p in this.Pawns)
+                {
+                    this.HealRandomInjury(p, 1f);
+                }
+            }
+            base.Tick();
+        }
+
+        private void HealRandomInjury(Pawn pawn, float points)
+        {
+            if ((from x in pawn.health.hediffSet.GetHediffs<Hediff_Injury>()
+                 where x.CanHealNaturally() || x.CanHealFromTending()
+                 select x).TryRandomElement(out Hediff_Injury hediff_Injury))
+            {
+                hediff_Injury.Heal(points);
+            }
+        }
+    }
+}
